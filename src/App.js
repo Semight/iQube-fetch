acfc04cdc9ee3d6b28c25f86232e9bec4638d9e0
semight <davidoluwasemiloorei@gmail.com>
2023-12-13 16:05:57 +0100

    fixing compile errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import store from './store';
-import CovidInfo from './CovidInfo';
+import store from '../src/store';
+import CovidInfo from './components/CovidInfo';
 
 const App = () => {
   return (
